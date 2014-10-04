5d2977109e2da3e14ed5179b9b1dca67530701a5
Paul Dornfeld <pdornfel@gmail.com>
2014-10-04 14:14:27 -0400

    formatted purse and date. can only update picks if current user is @user

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -5,7 +5,11 @@ class Tournament < ActiveRecord::Base
   has_many :results
 
   def pick(user)
-    Pick.find_by(tournament: self.id)
+    Pick.find_by(tournament: self.id, user: user)
+  end
+
+  def tournament_start_formatted
+    tournament_start.strftime('%b %d, %Y')
   end
 
   def has_not_started_yet
@@ -19,4 +23,31 @@ class Tournament < ActiveRecord::Base
     end
   end
 
+  def purse_formatted
+    "$#{separate_comma(self.purse)}"
+  end
+end
+
+private
+
+def separate_comma(number)
+  a = number.to_s.split('')
+  b = a.size/3.0
+  if a.size < 4
+    p number.to_s 
+  elsif a.size%3 == 0
+    n = -4
+    (b.to_i-1).times do |i|
+      a.insert(n, ',')
+      n -= 4
+    end
+    p a.join("")
+  else
+      n = -4
+    b.to_i.times do |i|
+      a.insert(n, ',')
+      n -= 4
+    end
+  p a.join("")
+  end
 end

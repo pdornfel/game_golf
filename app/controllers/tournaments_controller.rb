25403c9d17fc577a62746828e64a955105f8c74f
Paul Dornfeld <pdornfel@gmail.com>
2014-11-14 15:16:47 -0500

    fixed broken tournament links

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,7 +1,15 @@
 class TournamentsController < ApplicationController
-  
+
+	before_filter :get_group
+
   def show
     @tournament = Tournament.find(params[:id])
+  end
+
+  private
+
+  def get_group
+  	@group = Group.find(params[:group_id])
   end
 
 end
